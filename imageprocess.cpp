9e7a43706ee7ccab440fc750da7e9bd01f4ea37d
jgarciapft <jgarciapft@alumnos.unex.es>
2019-01-12 13:52:23 +0100

    - Corregida la iluminación estándar

--- imageprocess.cpp
+++ imageprocess.cpp
@@ -21,87 +21,80 @@
 #include "imageprocess.h"
 
 
-void imageprocess::rotar(uchar * imgO, uchar * imgD, float angle)
-{
-    short cw;
-    int sin1000, cos1000;
+void imageprocess::rotar(uchar *imgO, uchar *imgD, float angle) {
+	short cw;
+	int sin1000, cos1000;
 
-    sin1000 = sin(angle)*1000.;
-    cos1000 = cos(angle)*1000.;
+	sin1000 = sin(angle) * 1000.;
+	cos1000 = cos(angle) * 1000.;
 
-    asm volatile(
-        "fstcw %3\n\t"
-        "fwait\n\t"
-        "mov %3, %%ax\n\t"
-        "and $0xf3ff, %%ax\n\t"
-        "or $0x0c00, %%ax\n\t"
-        "push %%rax\n\t"
-        "fldcw (%%rsp)\n\t"
-        "pop %%rax\n\t"
-
-
-        //Insertar aquí el código del procedimiento
+	asm volatile(
+	"fstcw %3\n\t"
+	"fwait\n\t"
+	"mov %3, %%ax\n\t"
+	"and $0xf3ff, %%ax\n\t"
+	"or $0x0c00, %%ax\n\t"
+	"push %%rax\n\t"
+	"fldcw (%%rsp)\n\t"
+	"pop %%rax\n\t"
 
 
-        "fldcw %3\n\t"
+	//Insertar aquí el código del procedimiento
 
-        :
-        : "m" (imgO), "m" (imgD), "m" (angle), "m" (cw), "m" (sin1000), "m" (cos1000)
-        : "memory"
 
-    );
+	"fldcw %3\n\t"
+
+	:
+	: "m" (imgO), "m" (imgD), "m" (angle), "m" (cw), "m" (sin1000), "m" (cos1000)
+	: "memory"
+
+	);
 
 
 }
 
-void imageprocess::zoom(uchar * imgO, uchar * imgD, float s, int dx, int dy)
-{
-    short cw;
-    int sInt;
-    int ampliar;
+void imageprocess::zoom(uchar *imgO, uchar *imgD, float s, int dx, int dy) {
+	short cw;
+	int sInt;
+	int ampliar;
 
 
-    if(s>=1)
-    {
-        sInt = s;
-        ampliar = 1;
-    }
-    else
-    {
-        sInt = rint(1./s);
-        ampliar = 0;
-    }
+	if (s >= 1) {
+		sInt = s;
+		ampliar = 1;
+	} else {
+		sInt = rint(1. / s);
+		ampliar = 0;
+	}
 
-    asm volatile(
-        "fstcw %5\n\t"
-        "fwait\n\t"
-        "mov %5, %%ax\n\t"
-        "and $0xf3ff, %%ax\n\t"
-        "or $0x0c00, %%ax\n\t"
-        "push %%rax\n\t"
-        "fldcw (%%rsp)\n\t"
-        "pop %%rax\n\t"
+	asm volatile(
+	"fstcw %5\n\t"
+	"fwait\n\t"
+	"mov %5, %%ax\n\t"
+	"and $0xf3ff, %%ax\n\t"
+	"or $0x0c00, %%ax\n\t"
+	"push %%rax\n\t"
+	"fldcw (%%rsp)\n\t"
+	"pop %%rax\n\t"
 
-        //Insertar aquí el código del procedimiento
+	//Insertar aquí el código del procedimiento
 
 
-        "fldcw %5\n\t"
+	"fldcw %5\n\t"
 
-        :
-        : "m" (imgO), "m" (imgD), "m" (s), "m" (dx), "m" (dy), "m" (cw), "m" (sInt), "m" (ampliar)
-        : "memory"
+	:
+	: "m" (imgO), "m" (imgD), "m" (s), "m" (dx), "m" (dy), "m" (cw), "m" (sInt), "m" (ampliar)
+	: "memory"
 
-    );
+	);
 
 
 }
 
 
+void imageprocess::volteoHorizontal(uchar *imgO, uchar *imgD) {
 
-void imageprocess::volteoHorizontal(uchar * imgO, uchar * imgD)
-{
-
-    asm volatile(
+	asm volatile(
     "mov %0, %%rsi\n\t"					// Copia en %rsi la direccion de la imagen origen imgO
     "mov %1, %%rdi\n\t"					// Copia en %rdi la direccion de la imagen destino imgD
     "add $639, %%rsi\n\t"
@@ -133,10 +126,9 @@ void imageprocess::volteoHorizontal(uchar * imgO, uchar * imgD)
 
 }
 
-void imageprocess::volteoVertical(uchar * imgO, uchar * imgD)
-{
+void imageprocess::volteoVertical(uchar *imgO, uchar *imgD) {
 
-    asm volatile(
+	asm volatile(
 	"mov %0, %%rsi\n\t"					// Copia en %rsi la direccion de la imagen origen imgO
 	"mov %1, %%rdi\n\t"					// Copia en %rdi la direccion de la imagen destino imgD
 	"add $479*640, %%rsi\n\t"
@@ -169,77 +161,74 @@ void imageprocess::volteoVertical(uchar * imgO, uchar * imgD)
 }
 
 
-void imageprocess::iluminarLUT(uchar * tablaLUT, uchar gW)
-{
-    asm volatile(
-//	"mov %0, %%rbx\n\t"
-//	"mov $255, %%r8\n\t"
-//	"xor %%rcx, %%rcx\n\t"
-//	"bucleIL:\n\t"
-//		"mov %%rcx, %%rax\n\t"
-//		"mul %%r8b\n\t"
-//		"xor %%dx, %%dx\n\t"
-//		"cmpb $0, %1\n\t"
-//		"je divCeroIL\n\t"
-//		"divb %1\n\t"
-//		"jmp cargarResIL\n\t"
-//		"divCeroIL:\n\t"
-//  			"mov $255, %%al\n\t"
-//		"cargarResIL:\n\t"
-//			"mov %%al, (%%rbx, %%rcx)\n\t"
-//		"inc %%rcx\n\t"
-//		"cmp %1, %%cl\n\t"
-//		"jl bucleIL\n\t"
-//	"xor %%rcx, %%rcx\n\t"
-//  	"mov %1, %%cl\n\t"
-//	"bucleIL2:\n\t"
-//		"movb $255, (%%rbx, %%rcx)\n\t"
-//		"inc %%rcx\n\t"
-//		"cmp $256, %%rcx\n\t"
-//		"jl bucleIL2\n\t"
-		"\n\t"
+void imageprocess::iluminarLUT(uchar *tablaLUT, uchar gW) {
+
+	asm volatile(
+	"mov %0, %%rbx\n\t"
+	"xor %%rcx, %%rcx\n\t"
+	"xor %%rdx, %%rdx\n\t"
+ 	"mov %1, %%dl\n\t"
+	"bucleIL:\n\t"
+		"mov %%rcx, %%rax\n\t"
+		"imul $255, %%rax\n\t"
+		"cmp $0, %%dl\n\t"
+		"je divCeroIL\n\t"
+		"div %%dl\n\t"
+		"jmp cargarResIL\n\t"
+		"divCeroIL:\n\t"
+  			"mov $255, %%al\n\t"
+		"cargarResIL:\n\t"
+			"mov %%al, (%%rbx, %%rcx)\n\t"
+		"inc %%rcx\n\t"
+		"cmp %%rdx, %%rcx\n\t"
+		"jl bucleIL\n\t"
+  	"mov %%rdx, %%rcx\n\t"
+	"bucleIL2:\n\t"
+		"movb $255, (%%rbx, %%rcx)\n\t"
+		"inc %%rcx\n\t"
+		"cmp $256, %%rcx\n\t"
+		"jl bucleIL2\n\t"
 
 	:
 	: "m" (tablaLUT), "m" (gW)
-	: "%rax", "%rbx", "%rcx", "%rdx", "r8", "memory"
+	: "%rax", "%rbx", "%rcx", "%rdx", "memory"
 	);
 
 }
 
-void imageprocess::oscurecerLUT(uchar * tablaLUT, uchar gB)
-{
-    asm volatile(
-//	"mov %0, %%rbx\n\t"
-//	"xor %%rcx, %%rcx\n\t"
-//	"bucleOL:\n\t"
-//		"movb $0, (%%rbx, %%rcx)\n\t"
-//		"inc %%rcx\n\t"
-//		"cmp %1, %%cl\n\t"
-//		"jle bucleOL\n\t"
-//	"xor %%r8, %%r8\n\t"
-//	"xor %%rcx, %%rcx\n\t"
-//	"mov %1, %%cl\n\t"
-//	"inc %%cl\n\t"
-//	"bucleOL2:\n\t"
-//		"mov $255, %%r8\n\t"
-//		"sub %1, %%r8b\n\t"
-//		"mov %%rcx, %%rax\n\t"
-//		"sub %1, %%al\n\t"
-//		"mov $255, %%r9b\n\t"
-//		"mul %%r9b\n\t"
-//		"xor %%dx, %%dx\n\t"
-//		"cmp $0, %%r8\n\t"
-//		"je divCeroOL\n\t"
-//		"div %%r8w\n\t"
-//		"jmp cargarResOL\n\t"
-//		"divCeroOL:\n\t"
-//  			"mov $0, %%al\n\t"
-//		"cargarResOL:\n\t"
-//			"mov %%al, (%%rbx, %%rcx)\n\t"
-//		"inc %%rcx\n\t"
-//		"cmp $256, %%rcx\n\t"
-//		"jl bucleOL2\n\t"
-		"\n\t"
+void imageprocess::oscurecerLUT(uchar *tablaLUT, uchar gB) {
+
+	asm volatile(
+	"mov %0, %%rbx\n\t"
+	"xor %%rcx, %%rcx\n\t"
+	"bucleOL:\n\t"
+		"movb $0, (%%rbx, %%rcx)\n\t"
+		"inc %%rcx\n\t"
+		"cmp %1, %%cl\n\t"
+		"jle bucleOL\n\t"
+	"xor %%r8, %%r8\n\t"
+	"xor %%rcx, %%rcx\n\t"
+	"mov %1, %%cl\n\t"
+	"inc %%cl\n\t"
+	"bucleOL2:\n\t"
+		"mov $255, %%r8\n\t"
+		"sub %1, %%r8b\n\t"
+		"mov %%rcx, %%rax\n\t"
+		"sub %1, %%al\n\t"
+		"mov $255, %%r9b\n\t"
+		"mul %%r9b\n\t"
+		"xor %%dx, %%dx\n\t"
+		"cmp $0, %%r8\n\t"
+		"je divCeroOL\n\t"
+		"div %%r8w\n\t"
+		"jmp cargarResOL\n\t"
+		"divCeroOL:\n\t"
+  			"mov $0, %%al\n\t"
+		"cargarResOL:\n\t"
+			"mov %%al, (%%rbx, %%rcx)\n\t"
+		"inc %%rcx\n\t"
+		"cmp $256, %%rcx\n\t"
+		"jl bucleOL2\n\t"
 
 	:
 	: "m" (tablaLUT), "m" (gB)
@@ -249,39 +238,37 @@ void imageprocess::oscurecerLUT(uchar * tablaLUT, uchar gB)
 }
 
 
-void imageprocess::iluminarLUTMejorado(uchar * tablaLUT, uchar gW)
-{
-    asm volatile(
-        "\n\t"
+void imageprocess::iluminarLUTMejorado(uchar *tablaLUT, uchar gW) {
+
+	asm volatile(
+	"\n\t"
 
 
-        :
-        : "m" (tablaLUT), "m" (gW)
-        : "memory"
-    );
-
-}
-
-
-
-void imageprocess::oscurecerLUTMejorado(uchar * tablaLUT, uchar gB)
-{
-    asm volatile(
-        "\n\t"
-
-
-        :
-        : "m" (tablaLUT), "m" (gB)
-        : "memory"
-    );
+	:
+	: "m" (tablaLUT), "m" (gW)
+	: "memory"
+	);
 
 }
 
 
+void imageprocess::oscurecerLUTMejorado(uchar *tablaLUT, uchar gB) {
 
-void imageprocess::aplicarTablaLUT(uchar * tablaLUT, uchar * imgO, uchar * imgD)
-{
-    asm volatile(
+	asm volatile(
+	"\n\t"
+
+
+	:
+	: "m" (tablaLUT), "m" (gB)
+	: "memory"
+	);
+
+}
+
+
+void imageprocess::aplicarTablaLUT(uchar *tablaLUT, uchar *imgO, uchar *imgD) {
+
+	asm volatile(
 	"mov %0, %%rbx\n\t"
 	"mov %1, %%rsi\n\t"
 	"mov %2, %%rdi\n\t"
@@ -298,8 +285,7 @@ void imageprocess::aplicarTablaLUT(uchar * tablaLUT, uchar * imgO, uchar * imgD)
 		"jl bucleAL\n\t"
 
 	:
-//	: "m" (tablaLUT), "m" (imgO), "m" (imgD)
-	:  "m" (imgO), "m" (tablaLUT), "m" (imgD)
+	: "m" (tablaLUT), "m" (imgO), "m" (imgD)
 	: "%rbx", "%rcx", "%rsi", "%rdi", "r8", "memory"
 	);
 
